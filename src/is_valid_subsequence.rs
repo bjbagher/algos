72ac157457b804ad90a9d0cee5498f93b920f824
bb <bb@bbmbp.local>
2023-03-12 14:56:52 -0400

    fix subseq

--- src/is_valid_subsequence.rs
+++ src/is_valid_subsequence.rs
@@ -1,11 +1,15 @@
 pub fn is_valid_subsequence(seq: Vec<i32>, sub: Vec<i32>) -> bool {
-    // iterate
-    // check window
-    let seq_windows = seq.windows(sub.len());
-    for window in seq_windows {
-        if sub == window {
-            return true;
+    let mut sub = sub.iter();
+    let mut next_ref = sub.next();
+
+    for num in seq.iter() {
+        if Some(num) == next_ref {
+            next_ref = sub.next();
         }
+    }
+
+    if let None = next_ref {
+        return true;
     }
     false
 }
@@ -19,9 +23,20 @@ mod tests {
         let result = is_valid_subsequence(vec![2, 3, 4, 5, 6, 7, 8, 9, 9], vec![5, 6, 7]);
         assert_eq!(result, true);
     }
+
     #[test]
     fn is_valid_subsequence_2() {
+        let result = is_valid_subsequence(vec![2, 3, 4, 5, 6, 7, 8, 9, 9], vec![5, 6, 7]);
+        assert_eq!(result, true);
+    }
+    #[test]
+    fn is_valid_subsequence_3() {
         let result = is_valid_subsequence(vec![2, 3, 4, 5, 6, 7, 8, 9, 9], vec![5, 11, 7]);
         assert_eq!(result, false);
+    }
+    #[test]
+    fn is_valid_subsequence_4() {
+        let result = is_valid_subsequence(vec![5, 1, 22, 25, 6, -1, 8, 10], vec![]);
+        assert_eq!(result, true);
     }
 }
